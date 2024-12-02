f3415509ecd3b86ef2861d5e22edf36d803fc739
Ben Johnson <johben17796@gmail.com>
2024-12-01 22:13:31 -0600

    this is a lot

---
+++ Portfolio-Project/src/pages/PortfolioPage.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default function PortfolioPage () {
+    return (
+        "yes"
+    );
+}
+<body>
+    <header>
+        <h1>Johben28</h1>
+        <nav>
+            <ul>
+                <li><a href="#about-me">About Me</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#contact-me">Contact Me</a></li>
+                <li><a href="#resume">Resume</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="hero-banner"></section>
+
+return
+    <main class="page-section">
+        <section class="about-me">
+            <h2>About Me</h2>
+            <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+        </section>
+        <section class="flex-container">
+            <h2>Work</h2>
+            <section>
+                <section class="flex-item run-buddy">
+                    <header>Run Buddy</header>
+                    <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+                </section>
+
+                <section class="flex-item led-wall">
+                    <header>LED Wall</header>
+                    <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+                </section>
+                <section class="flex-item react-calc">
+                    <header>React Calc</header>
+                    <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+                </section>
+                <section class="flex-item pastel-puzzles">
+                    <header>Pastel Puzzles</header>
+                    <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+                </section>
+                <section class="flex-item surf-report">
+                    <header>Surf Report</header>
+                    <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+                </section>
+            </section>
+        </section>
+        <section class="contact">
+            <h2>Contact Me</h2>
+            <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+        </section>
+        <section class="resume">
+            <h2>Resume</h2>
+            <p>Lorem Ipsum I'm Ben I Rule This is placehlder</p>
+        </section>
+    </main>
+</body>
